dad5aac4fddf85b038db97c4dd5d09ca02d22d0c
Heber Lazo <heberlazo@gmail.com>
2023-05-11 23:27:51 -0500

    removed unnecessary code

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,9 +2,7 @@ class Subscription < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :preferences, presence: true
 
-  # PREFERENCES = %w[men women children]
-  PREFERENCES = ['men', 'women', 'children']
-
+  PREFERENCES = %w[men women children]
 
   validates :preferences, inclusion: { in: PREFERENCES, allow_blank: true }
 end
