f6a3f9efe66c1a65d0f3e4d45c6fba67e6b9b46d
H Lazo <67179149+Wusinho@users.noreply.github.com>
2023-05-12 16:44:59 -0500

    Dev ball 2 (#4)
    * Working language changer
    
    * Corrected form with languages
    
    * Corrected translation of form lang

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -1,2 +1,6 @@
 module SubscriptionsHelper
+  def fashion_preferences(pref, f)
+    f.label t("global.forms.#{pref}").to_sym
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  resources :subscriptions
-  root "home#index"
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    resources :subscriptions, only: [:create]
+    root "home#index"
+  end
 end
