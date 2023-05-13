c379014f00ddc8b55fbe8854100607a2e8b49204
Heber Lazo <heberlazo@gmail.com>
2023-05-13 14:54:45 -0500

    refactor templates

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -3,4 +3,15 @@ module SubscriptionsHelper
     f.label t("global.forms.#{pref}").to_sym
   end
 
+  def complete_subscription(survey)
+    if survey.completed
+      render 'surveys/completed_survey'
+    else
+      render 'questions/form', survey: survey
+    end
+  end
+
+
+
+
 end
