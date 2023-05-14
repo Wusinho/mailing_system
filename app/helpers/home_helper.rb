c702b13084d68683c547a2c0fcb40e61ef02292e
Heber Lazo <heberlazo@gmail.com>
2023-05-14 13:58:17 -0500

    change navabar to toggle

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -3,9 +3,13 @@ module HomeHelper
     def locale_links
       links = ""
       I18n.available_locales.each do |locale|
-        links += link_to(locale.to_s.upcase, root_path(locale: locale), class: 'nav-link')
+        links += link_to(locale.to_s.upcase, root_path(locale: locale), class: green_btn)
       end
       links.html_safe
+    end
+
+    def green_btn
+      'btn btn-success'
     end
 
 end
