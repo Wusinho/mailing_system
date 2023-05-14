f9d8e6facf62b3c47dd5024bc55a0830ec5aa3fb
Heber Lazo <heberlazo@gmail.com>
2023-05-13 22:05:04 -0500

    moved condition to helper

--- app/helpers/surveys_helper.rb
+++ app/helpers/surveys_helper.rb
@@ -1,2 +1,15 @@
 module SurveysHelper
+
+  def render_question_form(form)
+    if form.object.question.question_type == 'long_answer'
+      form.label(form.object.question.question) +
+        form.text_field(:answer) +
+        form.hidden_field(:question_id, value: form.object.question.id)
+    else
+      form.label(form.object.question.question) +
+        form.select(:answer, form.object.question.alternatives, {}) +
+        form.hidden_field(:question_id, value: form.object.question.id)
+    end
+  end
+
 end
